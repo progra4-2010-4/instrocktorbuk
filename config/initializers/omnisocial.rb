fe7105f35935dde492ba178bedc61a5787f2d79a
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-22 21:25:06 -0600

    LO HICE LOGIN CON FACEBOOK O TWITTER PARECE Q FUNCIONA SOLO ES DE PROBARLO

--- config/initializers/omnisocial.rb
+++ config/initializers/omnisocial.rb
@@ -7,13 +7,14 @@ Omnisocial.setup do |config|
   # config.facebook 'APP_KEY', 'APP_SECRET', :scope => 'publish_stream'
   
   if Rails.env.production?
-    
-    # Configs for production mode go here
-    
+     # Configs for production mode go here
+     config.twitter 'FQx86BCrCNNOUAJ2fmbRA','ITZCWiiYUYaeU1vZNaWBJtKZyinFt5U6Wdfcy409IY' 
+     config.facebook 'a39a76dbf755337458de46a1578c3015','1b52cc35595810accae3f631de8b7790'   
   elsif Rails.env.development?
     
     # Configs for development mode go here
-    
+    config.twitter 'FQx86BCrCNNOUAJ2fmbRA','ITZCWiiYUYaeU1vZNaWBJtKZyinFt5U6Wdfcy409IY'
+    config.facebook 'a39a76dbf755337458de46a1578c3015','1b52cc35595810accae3f631de8b7790'
   end
   
 end
